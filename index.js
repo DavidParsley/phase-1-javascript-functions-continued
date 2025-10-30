6f672ceaa70aa570b1630b4f61b946aca815f2da
David Parsley <davidparsley.kakhayanga@gmail.com>
2025-10-30 20:26:17 +0300

    Transferred from main account

--- index.js
+++ index.js
@@ -13,3 +13,24 @@ console.log(saturdayFun())
       return `You are ${flair}${adjective}${flair}!`;
     }
 } 
+
+Example Calls
+
+Here are some example calls for each function:
+
+    Calling saturdayFun:
+
+console.log(saturdayFun()); // "This Saturday, I want to roller-skate!"
+console.log(saturdayFun("hike")); // "This Saturday, I want to hike!"
+
+Calling mondayWork:
+
+console.log(mondayWork()); // "This Monday, I will go to the office."
+console.log(mondayWork("work from home")); // "This Monday, I will work from home."
+
+Calling wrapAdjective:
+
+const encouragingPromptFunction = wrapAdjective("!!!");
+console.log(encouragingPromptFunction("a dedicated programmer")); // "You are !!!a dedicated programmer!!!"
+console.log(wrapAdjective("%")("a dedicated programmer")); // "You are %a dedicated programmer%!"
+
